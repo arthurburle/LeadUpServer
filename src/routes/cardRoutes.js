3c52822bd0bf27a0d236a18767ec2c5fe0e8dbad
ArthurBurle <arthurburle@gmail.com>
2021-01-25 04:25:57 -0300

    editar e deletar

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -31,4 +31,33 @@ router.post('/cards', async (req, res) => {
   }
 });
 
+router.put('/cards/:id', async (req, res) => {
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a title and a description' });
+  }
+
+  try {
+    const card = await Card.findByIdAndUpdate(
+      { _id: req.params.id },
+      { title, description, userId: req.user._id }
+    );
+    res.send(card);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
+router.delete('/cards/:id', async (req, res) => {
+  try {
+    const card = await Card.findByIdAndRemove({ _id: req.params.id });
+    res.send(card);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
 module.exports = router;
